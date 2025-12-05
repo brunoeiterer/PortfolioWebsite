8f8e2f2841d29b6645256ac1e19f78d9a32d0f75
brunoeiterer <brunoeiterer@gmail.com>
2025-12-05 13:46:09 -0300

    Add hero section

--- src/components/HeroSection/HeroSection.styles.tsx
+++ src/components/HeroSection/HeroSection.styles.tsx
@@ -8,13 +8,7 @@ export const HeroSectionContainer = styled.div`
     grid-template-columns: 1fr 1fr 1fr 1fr;
     grid-column: 1 / span 4;
 
-    margin-left: 1rem;
-    margin-right: 1rem;
     margin-top: 1rem;
-
-    height: 269px;
-
-    border: 1px solid black;
 
     @media (min-width: 768px) {
         grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
@@ -27,6 +21,53 @@ export const HeroSectionContainer = styled.div`
     @media (min-width: 1024px) {
         grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
         grid-column: 1 / span 12;
+    }
+`;
+
+export const HeroTextTitle = styled.h1`
+    font-size: 26px;
+    width: 100%;
+    grid-row: 1;
+    grid-column: 1 / 4;
+
+    margin-left: 0.5rem;
+
+    @media (min-width: 768px) {
+        font-size: 2rem;
+        margin-left: 0.5rem;
+    }
+
+    @media (min-width: 1024px) {
+        grid-column: 1 / span 12;
+        margin-left: 3.5rem;
+        font-size: 4rem;
+    }
+`;
+
+export const HeroTextContainer = styled.div`
+    grid-row: 1;
+    grid-column: 1 / span 2;
+    margin-left: 1rem;
+    margin-top: 2.5rem;
+
+    max-width: 80%;
+
+    font-size: 0.5625rem;
+
+    @media (min-width: 768px) {
+        grid-column: 1 / span 4;
+
+        font-size: 1rem;
+
+        margin-left: 1.5rem;
+    }
+
+    @media (min-width: 1024px) {
+        grid-column: 1 / span 8;
+
+        font-size: 1.5rem;
+
+        margin-top: 7rem;
 
         margin-left: 5rem;
         margin-right: 5rem;
@@ -35,25 +76,35 @@ export const HeroSectionContainer = styled.div`
 
 export const HeroImage = styled(Image)`
     grid-row: 1;
-    grid-column: 1 / 5;
-    margin: 4rem 1rem 0 1rem;
+    grid-column: 1 / span 4;
+    margin: 1rem 1rem 0 1rem;
     max-width: 90%;
     height: auto;
-    margin-bottom: -1rem;
+
+    
+    @media (min-width: 768px) {
+        grid-column: 1 / span 8;
+
+        margin-left: 1.5rem;
+    }
+
+    @media (min-width: 1024px) {
+        grid-column: 4 / span 7;
+
+        max-width: 100%;
+
+        margin-top: -1rem;
+        margin-left: 5rem;
+        margin-right: 5rem;
+    }
 `;
 
 export const HeroSectionSeparator = styled.hr`
-    grid-column: 1 / 5;
+    width: 110%;
+    grid-column: 1 / -1;
     color: #00000096;
     filter: blur(2px);
-`;
-
-export const HeroTextTitleContainer = styled.div`
-    width: 100%;
-    grid-row: 1;
-    grid-column: 1 / 4;
-
-    margin: 1em;
+    margin-left: -3rem;
 `;
 
 export const HeroHighlightsContainer = styled.div`
@@ -65,9 +116,4 @@ export const HeroHighlightsContainer = styled.div`
 
     margin: 4rem 0 0 1.5rem;
     font-size: 10px;
-`;
-
-export const HeroTextTitle = styled.h1`
-    font-size: 26px;
-    margin: 1rem 1rem 1rem 0;
 `;
--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,4 +1,4 @@
-import {HeroTextTitle, HeroImage, HeroSectionSeparator, HeroTextTitleContainer, HeroHighlightsContainer, HeroSectionContainer } from "./HeroSection.styles";
+import {HeroTextTitle, HeroImage, HeroSectionSeparator, HeroHighlightsContainer, HeroSectionContainer, HeroTextContainer } from "./HeroSection.styles";
 
 interface HeroSectionProps extends React.HTMLAttributes<HTMLElement> {
 
@@ -7,18 +7,32 @@ interface HeroSectionProps extends React.HTMLAttributes<HTMLElement> {
 export default function HeroSection({ id }: HeroSectionProps) {
     return (
         <HeroSectionContainer id={id}>
-            { 'Hero' }
-        </HeroSectionContainer>
-    )
-
-    return (
-        <>
+            <HeroTextTitle>
+                Hi, I'm Bruno :)
+            </HeroTextTitle>
+            <HeroTextContainer>
+                <ul>
+                    <li>Generalist Software Engineer</li>
+                    <li>7 years of experience</li>
+                    <li>Enterprise • Embedded • Web • Desktop</li>
+                    <li>Strong C#, C</li>
+                    <li>Mid TypeScript, React, C++</li>
+                    <li>Also: Python, Java</li>
+                </ul>
+            </HeroTextContainer>
             <HeroImage
                 src='/images/HeroImage.png'
                 alt='developing with banshee'
                 width={953}
                 height={768}
             />
+            <HeroSectionSeparator />
+        </HeroSectionContainer>
+    )
+
+    return (
+        <>
+
 
             <HeroTextTitleContainer>
                 <HeroTextTitle>
