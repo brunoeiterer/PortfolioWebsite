e35a05f1bbb5d26ec87bf9f3560f5f9b78e43683
brunoeiterer <brunoeiterer@gmail.com>
2025-12-12 13:27:37 -0300

    Add Contact section

--- src/components/Contact/Contact.styles.tsx
+++ src/components/Contact/Contact.styles.tsx
@@ -3,25 +3,27 @@
 import styled from 'styled-components';
 
 export const ContactContainer = styled.div`
-    display: grid;
-
-    grid-template-columns: 1fr 1fr 1fr 1fr;
+    display: flex;
+    flex-wrap: wrap;
 
     grid-column: 1 / span 4;
 
-    height: 200px;
-
     margin-top: 1rem;
 
-    border: 1px solid black;
+    gap: 2rem;
 
     @media (min-width: 768px) {
-        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
         grid-column: 1 / span 8;
     }
 
     @media (min-width: 1024px) {
-        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
         grid-column: 1 / span 12;
     }
+`;
+
+export const ContactItem = styled.div`
+    display: flex;
+    gap: 0.5rem;
+
+    align-items: center;
 `;
--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,9 +1,29 @@
-import { ContactContainer } from "./Contact.styles";
+import { ContactContainer, ContactItem } from "./Contact.styles";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Contact() {
     return (
         <ContactContainer>
-            { 'Contact information' }
+            <ContactItem>
+                <Image src='/images/LinkedinIcon.svg' alt='LinkedinIcon' height={24} width={24} />
+                <Link href='https://www.linkedin.com/in/brunoeiterer/' target='_blank'>brunoeiterer</Link>
+            </ContactItem>
+
+            <ContactItem>
+                <Image src='/images/EmailIcon.svg' alt='EmailIcon' height={24} width={24} />
+                <a href='mailto:brunoeiterer@gmail.com'>brunoeiterer@gmail.com</a>
+            </ContactItem>
+
+            <ContactItem>
+                <Image src='/images/GithubIcon.svg' alt='GithubIcon' height={24} width={24} />
+                <a href='https://github.com/brunoeiterer/' target='_blank'>brunoeiterer</a>
+            </ContactItem>
+
+            <ContactItem>
+                <Image src='/images/WhatsappIcon.svg' alt='whatsappbIcon' height={24} width={24} />
+                <a href='https://wa.me/5548996118470/' target='_blank'>+55 (48) 99611-8470</a>
+            </ContactItem>
         </ContactContainer>
     )
 }
